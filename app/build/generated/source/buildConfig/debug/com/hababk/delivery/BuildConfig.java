570517043a8965fc395f1b25d952b58cb56dfa5b
Tayeb-Ali <Mr.elteb@gmail.com>
2019-02-25 10:52:48 +0200

    fix Layoute

--- app/build/generated/source/buildConfig/debug/com/hababk/delivery/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/hababk/delivery/BuildConfig.java
@@ -5,7 +5,7 @@ package com.hababk.delivery;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.hb_food";
+  public static final String APPLICATION_ID = "com.hababk.delivery";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
